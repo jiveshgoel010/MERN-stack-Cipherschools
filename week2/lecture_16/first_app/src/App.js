3cc9eb4970d0fe19a46ec44b4dda996b9e5161aa
jiveshgoel010 <jiveshgoel010@gmail.com>
2024-07-22 01:09:59 +0530

    Added files

---
+++ week2/lecture_16/first_app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name="Jivesh"
+  return (
+    <div>
+      <h1 style={{
+        color:'red',
+        textAlign: "center"
+        }}>
+        Hello from {name}
+      </h1>
+    </div>
+    
+  );
+}
+
+export default App;
